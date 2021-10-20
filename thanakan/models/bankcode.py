17c125d77e1a1440d1a8d51b64cb0b43749be912
Nutchanon Ninyawee <nutchanon@codustry.com>
2021-10-20 21:02:41 +0700

    add note about bank code

--- thanakan/models/bankcode.py
+++ thanakan/models/bankcode.py
@@ -4,6 +4,9 @@ from pydantic.types import constr
 
 
 class BankCode(Enum):
+    """
+    https://www.bot.or.th/Thai/Statistics/DataManagementSystem/Standard/StandardCode/Pages/default.aspx
+    """
     BBL = "002"
     KBANK = "004"
 
